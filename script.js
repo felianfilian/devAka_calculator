1b154e3098ac346286d92f0bfbaec84349134e84
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-02-23 19:49:00 +0100

    calculate

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function appendOperation(operation) {
   document.getElementById("result-area").innerHTML += operation;
 }
+
+function getResult() {
+  let resultContainer = document.getElementById("result-area");
+  let result = eval(resultContainer.innerHTML);
+  resultContainer.innerHTML = result;
+}
