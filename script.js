1eb5e718044c44063b2c372d31c4f2f0a8d65816
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-02-23 19:59:05 +0100

    delete function

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function getResult() {
   let result = eval(resultContainer.innerHTML);
   resultContainer.innerHTML = result;
 }
+
+function deleteLast() {
+  let resultContainer = document.getElementById("result-area");
+  if (resultContainer.innerHTML.endsWith(" ")) {
+    resultContainer.innerHTML = resultContainer.innerHTML.slice(0, -3);
+  } else {
+    resultContainer.innerHTML = resultContainer.innerHTML.slice(0, -1);
+  }
+}
